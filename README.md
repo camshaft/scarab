b75f1d0141d3944dc8cda3113eaa9bb427b8b411
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-05-28 14:27:11 -0600

    fix ci setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scarab [![Build Status](https://travis-ci.org/camshaft/scarab.svg?branch=master)](https://travis-ci.org/camshaft/scarab)
+
 content-addressable file storage for elixir
--- test/scarab_test.exs
+++ test/scarab_test.exs
@@ -1,62 +1,72 @@
-defmodule Test.Scarab.Backend.FS do
+defmodule Test.Scarab do
   use ExUnit.Case
-
-  alias __MODULE__.Repo
 
   setup_all do
     Application.ensure_all_started(:ex_aws)
     Application.ensure_all_started(:httpoison)
 
-    defmodule Repo do
+    defmodule FS do
       dir = :os.timestamp |> :erlang.phash2 |> to_string
       @path Path.join(System.tmp_dir!, dir)
 
+      use Scarab, backend: Scarab.Backend.FS,
+        config: %{path: @path}
+    end
+
+    defmodule S3 do
       use Scarab, backend: Scarab.Backend.S3,
-                  config: %{path: @path, bucket: "foo"}
+                  config: %{bucket: "foo"}
     end
 
     :ok
   end
 
-  test "Repo.put(content)" do
-    content = gen_content()
+  for backend <- [FS, S3] do
+    impl = Module.concat(__MODULE__, backend)
 
-    {:ok, hash} = Repo.put(content)
-    {:ok, ^content} = Repo.get(hash)
-  end
+    test "#{inspect(backend)} - Repo.put(content)" do
+      impl = unquote(impl)
+      content = gen_content()
 
-  test "Repo.link(namespace, name, hash)" do
-    content = gen_content()
-    namespace = gen_namespace()
-    name = "link"
+      {:ok, hash} = impl.put(content)
+      {:ok, ^content} = impl.get(hash)
+    end
 
-    {:ok, hash} = Repo.put(content)
-    :ok = Repo.link(namespace, name, hash)
-    {:ok, ^content} = Repo.resolve(namespace, name)
-    {:ok, ^content} = Repo.resolve(namespace, hash)
-    :ok = Repo.unlink(namespace, name)
-    {:error, :enoent} = Repo.resolve(namespace, name)
-  end
+    test "#{inspect(backend)} - Repo.link(namespace, name, hash)" do
+      impl = unquote(impl)
+      content = gen_content()
+      namespace = gen_namespace()
+      name = "link"
 
-  test "Repo.link(namespace, name, ref)" do
-    content = gen_content()
-    namespace = gen_namespace()
-    name1 = "nested-link1"
-    name2 = "nested-link2"
+      {:ok, hash} = impl.put(content)
+      :ok = impl.link(namespace, name, hash)
+      {:ok, ^content} = impl.resolve(namespace, name)
+      {:ok, ^content} = impl.resolve(namespace, hash)
+      :ok = impl.unlink(namespace, name)
+      {:error, :enoent} = impl.resolve(namespace, name)
+    end
 
-    {:ok, hash} = Repo.put(content)
-    :ok = Repo.link(namespace, name1, hash)
-    :ok = Repo.link(namespace, name2, name1)
-    {:ok, ^content} = Repo.resolve(namespace, name1)
-    {:ok, ^content} = Repo.resolve(namespace, name2)
-    {:ok, ^content} = Repo.resolve(namespace, hash)
+    test "#{inspect(backend)} - Repo.link(namespace, name, ref)" do
+      impl = unquote(impl)
+      content = gen_content()
+      namespace = gen_namespace()
+      name1 = "nested-link1"
+      name2 = "nested-link2"
 
-    :ok = Repo.unlink(namespace, name1)
-    {:error, :enoent} = Repo.resolve(namespace, name1)
-    {:error, :enoent} = Repo.resolve(namespace, name2)
+      {:ok, hash} = impl.put(content)
+      :ok = impl.link(namespace, name1, hash)
+      :ok = impl.link(namespace, name2, name1)
+      {:ok, ^content} = impl.resolve(namespace, name1)
+      {:ok, ^content} = impl.resolve(namespace, name2)
+      {:ok, ^content} = impl.resolve(namespace, hash)
 
-    :ok = Repo.unlink(namespace, name2)
-    {:error, :enoent} = Repo.resolve(namespace, name2)
+      :ok = impl.unlink(namespace, name1)
+      {:error, :enoent} = impl.resolve(namespace, name1)
+      {:error, :enoent} = impl.resolve(namespace, name2)
+
+      :ok = impl.unlink(namespace, name2)
+      {:error, :enoent} = impl.resolve(namespace, name2)
+    end
   end
 
   defp gen_namespace() do
